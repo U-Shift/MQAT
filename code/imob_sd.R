1f8bc1d2a2025b50b08f9a0439d05ca3044488dd
RFlx <temospena@gmail.com>
2023-09-15 14:29:39 +0100

    censos 2021 freguesias

---
+++ code/imob_sd.R
@@ -0,0 +1,19 @@
+# dicofre 21
+
+# rendimento médio freguesia alojamento
+
+# nº veiculos origem alojamento
+
+# ter carta de condução %
+
+# % genero em e total, pelos censos
+
+# ocupação: trabalhador fora de casa, estudante, reformado e outros
+
+# ter passe social
+
+# ter estacionamento no local de trabalho
+
+# distancia viagem por modo
+# duração viagem
+
