352be4e79d45b4cf466261fb6cdd26183f48cc3a
RFlx <temospena@gmail.com>
2023-10-13 17:23:55 +0100

    geo trips

--- code/dataprep/geo_data_aml.R
+++ code/dataprep/geo_data_aml.R
@@ -6,14 +6,43 @@ st_write(FREGUESIASgeo, "geo/FREGUESIASgeo.gpkg")
 
 # municipios geo
 MUNICIPIOSgeo = readRDS(url("https://github.com/U-Shift/biclar/releases/download/0.0.1/MUNICIPIOSgeo.Rds"))
-st_write(MUNICIPIOSgeo, "geo/MUNICIPIOSgeo.gpkg")
+MUNICIPIOSgeo$Concelho[MUNICIPIOSgeo$Concelho == "Setubal"] = "Setúbal"
+st_write(MUNICIPIOSgeo, "geo/MUNICIPIOSgeo.gpkg", delete_dsn = TRUE)
 
 # centroids
 MUNICIPIOScentroid = st_centroid(MUNICIPIOSgeo) |> st_transform(3857)
 mapview::mapview(MUNICIPIOScentroid)
-st_write(MUNICIPIOScentroid, "geo/MUNICIPIOScentroid.gpkg")
+st_write(MUNICIPIOScentroid, "geo/MUNICIPIOScentroid.gpkg", delete_dsn = TRUE)
 
 # population weighted centroids
 # homework!
 # tips: use bgri and population or households or buildings
 # what are the differences between the results and the geometric centroid?
+
+
+
+# Join Trips with geometric attributes
+library(tidyverse)
+library(sf)
+TRIPSgeo_mun = TRIPSmode_mun |>
+  group_by(Origin_mun) |> 
+  summarise_if(is.numeric, sum) |> 
+  rename(Concelho = Origin_mun) |>
+  left_join(MUNICIPIOSgeo) |> 
+  st_as_sf()
+
+mapview::mapview(TRIPSgeo_mun, zcol = "Bike")
+
+st_write(TRIPSgeo_mun, "geo/TRIPSgeo_mun.gpkg", delete_dsn = TRUE)
+
+
+TRIPSgeo_freg = TRIPSmode_freg |>
+  group_by(Origin_dicofre16) |> 
+  summarise_if(is.numeric, sum) |> 
+  rename(Dicofre = Origin_dicofre16) |>
+  left_join(FREGUESIASgeo) |> 
+  st_as_sf()
+
+mapview::mapview(TRIPSgeo_freg, zcol = "Bike")
+
+st_write(TRIPSgeo_freg, "geo/TRIPSgeo_freg.gpkg")
